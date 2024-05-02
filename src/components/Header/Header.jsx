f9f7ef88d8e7fd55d83f41b77d925aee6df73f5d
Micheal Oshogwe <kenneth.oshogwe@gmail.com>
2024-05-02 09:36:38 +0100

    creating a food-app

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-content">
+        <h2>Order your Favourite Food here</h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur fuga
+          ducimus quidem inventore dolores nesciunt.
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
