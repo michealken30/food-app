88dc6803657c6600f5c3f65c3675f153d3ba3794
Micheal Oshogwe <kenneth.oshogwe@gmail.com>
2024-05-05 21:57:19 +0100

    created the api end point for add, delete and lsit food items

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoutes.js";
 
 const app = express();
 const port = 4000;
@@ -9,6 +10,9 @@ app.use(express.json());
 app.use(cors());
 
 connectDB();
+
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
 
 app.get("/", (req, res) => {
   res.send("Api working");
