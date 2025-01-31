31fe2b4b57f652aa86e5032eecf48ddcb4738b09
michealken30 <kenneth.oshogwe@gmail.com>
2025-01-31 21:28:27 +0100

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-export const Db = async () => {
+export const connectDB = async () => {
   await mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log("database connected ");
   });
