894ac23c679b211429345a6a6beca2418190de2d
michealken30 <kenneth.oshogwe@gmail.com>
2025-01-31 21:10:36 +0100

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,7 @@
 import mongoose from "mongoose";
 
-export const connectDB = async () => {
-  await mongoose
-    .connect(
-      "mongodb+srv://kennethoshogwe2:ORvShSK7FfIo2Xov@cluster0.ldkoji3.mongodb.net/food-del"
-    )
-    .then(() => console.log("db connected"));
+export const Db = async () => {
+  await mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log("database connected ");
+  });
 };
