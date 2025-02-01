0d91241f5cd83379461d182df4501becfa8d0c7f
michealken30 <kenneth.oshogwe@gmail.com>
2025-02-01 21:15:47 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,33 +26,33 @@ app.get("/", (req, res) => {
   res.send("Api working");
 });
 
-// Basic health check to see if the server is running
-app.get("/healthz", (req, res) => {
-  res.status(200).send("Healthy");
-});
+// // Basic health check to see if the server is running
+// app.get("/healthz", (req, res) => {
+//   res.status(200).send("Healthy");
+// });
 
-let lastReadyState = null;
-// Readiness check to see if the server is ready to serve requests
-app.get("/ready", (req, res) => {
-  // Here you can add logic to check database connection or other dependencies
-  const isDbConnected = mongoose.connection.readyState === 1;
-  if (isDbConnected !== lastReadyState) {
-    console.log(`Database readyState: ${mongoose.connection.readyState}`);
-    lastReadyState = isDbConnected;
-  }
+// let lastReadyState = null;
+// // Readiness check to see if the server is ready to serve requests
+// app.get("/ready", (req, res) => {
+//   // Here you can add logic to check database connection or other dependencies
+//   const isDbConnected = mongoose.connection.readyState === 1;
+//   if (isDbConnected !== lastReadyState) {
+//     console.log(`Database readyState: ${mongoose.connection.readyState}`);
+//     lastReadyState = isDbConnected;
+//   }
 
-  if (isDbConnected) {
-    res.status(200).send("Ready");
-  } else {
-    res.status(503).send("Not Ready");
-  }
-});
+//   if (isDbConnected) {
+//     res.status(200).send("Ready");
+//   } else {
+//     res.status(503).send("Not Ready");
+//   }
+// });
 
-// Startup check to ensure the server has started correctly
-app.get("/started", (req, res) => {
-  // Assuming the server has started correctly if this endpoint is reachable
-  res.status(200).send("Started");
-});
+// // Startup check to ensure the server has started correctly
+// app.get("/started", (req, res) => {
+//   // Assuming the server has started correctly if this endpoint is reachable
+//   res.status(200).send("Started");
+// });
 
 app.listen(port, () => {
   console.log(`Server Started on http://localhost:${port}`);
