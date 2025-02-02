5098b9e812cfbdb236b8bd00b05b7b8cbab99667
michealken30 <kenneth.oshogwe@gmail.com>
2025-02-02 22:51:24 +0100

    Update StoreContext.jsx

--- src/Context/StoreContext.jsx
+++ src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "http://michealken30.com.ng/";
   const [token, setToken] = useState("");
 
   const [food_list, setFoodList] = useState([]);
