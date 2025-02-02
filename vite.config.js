1d733c7b51edbe81e90f1c1f9b650bb3eaf2fefc
michealken30 <kenneth.oshogwe@gmail.com>
2025-02-02 22:26:18 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    host: true,
+    port: 8080, // Ensure it matches your frontend service
+    strictPort: true
+  },
+  preview: {
+    allowedHosts: ['michealken30.com.ng']
+  }
+});
